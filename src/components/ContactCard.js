86201f2b589e55cbfb2bdd77f3e20e7dfcc69919
Cristian Sánchez <cristianrestrepo13@gmail.com>
2024-10-03 21:17:37 -0500

    Contact card problems solved

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -2,6 +2,9 @@ import React from "react";
 import styled from "styled-components";
 
 const CardContainer = styled.div`
+  * {
+    box-sizing: border-box;
+  }
   color: white;
   background-color: ${(props) => props.theme.colors.yellow};
   width: 100%;
@@ -25,26 +28,12 @@ const InputGroup = styled.div`
   justify-content: space-between;
   align-items: center;
   max-width: 500px;
-
-  /* Media query para pantallas más grandes */
-  @media (min-width: 768px) {
-    gap: 16px; /* Espaciado entre campos */
-  }
-
-  /* Media query para dispositivos móviles */
-  @media (max-width: 768px) {
-    flex-direction: column;
   }
 `;
 
 const InputContainer = styled.div`
   width: 48%;
   margin-bottom: 16px;
-
-  /* Para dispositivos móviles, los inputs se extienden al 100% */
-  @media (max-width: 768px) {
-    width: 100%;
-  }
 `;
 
 const InputField = styled.input`
@@ -56,14 +45,8 @@ const InputField = styled.input`
   padding: 10px 20px;
   border: 1px solid #ccc;
   border-radius: 4px;
-  font-size: ${(props) => props.theme.fontSizes.normal};
-
   ::placeholder {
     color: #999;
-  }
-
-  @media (max-width: 768px) {
-    font-size: ${(props) => props.theme.fontSizes.small};
   }
 `;
 
@@ -87,15 +70,6 @@ const TextArea = styled.textarea`
 
   ::placeholder {
     color: #999;
-  }
-
-  @media (max-width: 768px) {
-    border-radius: 4px; /* Se ajusta la forma para móviles */
-    width: 90%;
-    text-align: center;
-    text-justify: center;
-    padding: 0px;
-    margin: 5px 10px;
   }
 `;
 
